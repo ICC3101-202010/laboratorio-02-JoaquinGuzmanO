7c3e7910741cb7d95f2af1c594ca8e3592313cbc
Joaco Guzmán <jaguzman1@miuandes.cl>
2020-03-28 19:46:30 -0300

    constructor

---
+++ laboratorio1/laboratorio1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace laboratorio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
